c14a7360c539f7ac0dc9c1cd33b66cba515a8989
Oliviagiles09 <giles11@usf.edu>
2025-02-16 22:12:56 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calcualteSalary(baseSalary, bonus, taxRate) {
 
 consol.log(calculateSalary(5000, 500, 0.1));
 consol.log(calculateSalary(7000, 1000, 0.15));
+
+// Task 2
+function calculateDiscount(price, discountedRate) {
+  let finalPrice = price - (price * discountRate);
+  return 'Final Price: $${finalPrice.toFixed(2)}';
+}
+
+consol.log(calculateDiscount(100, 0.2));
+consol.log(calculateDiscount(250, 0.15));
